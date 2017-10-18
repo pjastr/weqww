b416a5de48de4f82d920b9660a3f0254541d2c52
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-18 17:13:53 +0200

    Add files via upload

---
+++ ConsoleApplication22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba ludzik1 = new Osoba();
+            ludzik1.imie = "Anna";
+            ludzik1.nazwisko = "Nowak";
+            ludzik1.plec = "K";
+            ludzik1.Info();
+
+            Console.ReadKey();
+        }
+    }
+}
