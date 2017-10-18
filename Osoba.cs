184dffff60036365fa2e86624e6c190650d799ed
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-18 17:10:02 +0200

    Add files via upload

---
+++ Osoba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+    class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public string plec;
+
+        public void Info()
+        {
+            if (plec == "K")
+            {
+                Console.WriteLine("{0} {1} jest kobietą", imie, nazwisko);
+            }
+            else if (plec == "M")
+            {
+                Console.WriteLine("{0} {1} jest mężczyzną", imie, nazwisko);
+                
+            }
+        }
+    }
+}
